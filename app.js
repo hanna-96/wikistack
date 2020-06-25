05651f601dc5fd0bfdbb44ae96c787bb00ef27da
Hanna Rzheutskaya <a.rzheutskaya96@gmail.com>
2020-06-25 14:40:11 -0400

    pushed wikictack

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+let morgan = require('morgan');
+
+let app = express();
+let htmlfiles = require('./views/main')
+
+
+app.use(morgan('dev'));
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get('/',(req,res,next)=>{
+    res.send(htmlfiles());
+})
+
+let PORT = 3000;
+
+app.listen(PORT,()=>{
+    console.log('App is listening');
+})
